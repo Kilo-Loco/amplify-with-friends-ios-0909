c0d265cfac2cd9ec84b53688335edfa3e7bc8150
Kilo Loco <kiloloco@me.com>
2020-09-09 11:20:24 -0700

    Initial Commit

---
+++ uploadin some stuff/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uploadin some stuff
+//
+//  Created by Kyle Lee on 9/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
